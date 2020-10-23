66e99a81db4422ef33e8ad19a71582cb3a06aa04
elyzinger <45950112+elyzinger@users.noreply.github.com>
2020-10-23 13:05:44 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   firebase:{
-  // Your web app's Firebase configuration
-    apiKey: "AIzaSyDSWH2FzbUFJtDnY_K6xT-t-PdZgQ60JKk",
-    authDomain: "shopping-list-17adb.firebaseapp.com",
-    databaseURL: "https://shopping-list-17adb.firebaseio.com",
-    projectId: "shopping-list-17adb",
-    storageBucket: "shopping-list-17adb.appspot.com",
-    messagingSenderId: "139013367285",
-    appId: "1:139013367285:web:156b76c1af7ef9d730acbc"
+
   }
 };
 
